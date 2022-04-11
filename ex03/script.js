c234557b3ebf44671159c626dce3c0b58a29b9e9
Stevens Wendell <stevench10@outlook.com>
2022-04-11 17:37:27 -0300

    subindo versão final da atividade 3

--- ex03/script.js
+++ ex03/script.js
@@ -1,4 +1,6 @@
-let cont = 0;
+let idDiv = 0
+let idBt = 0
+let cont = 0
 
 function gerarCor() {
     const letters = '0123456789ABCDEF';
@@ -14,22 +16,65 @@ function gerarCor() {
 
 function gerarDiv(){
     let div = document.createElement('div')
-    div.setAttribute('background-color', gerarCor())
-    div.setAttribute('margin', '10px')
-    div.setAttribute('id', `${cont++}`)
-    div.setAttribute('onclick', 'criarContador()')
+    div.setAttribute('style', `background-color: ${gerarCor()}`)
+    div.setAttribute('id', `${idDiv+1}`)
+    div.setAttribute('onclick', 'contar(id)')
+    div.setAttribute('cont', `0`)
+    div.setAttribute('txt', receberInput().value)
 
+    idDiv += 1
+    
+    document.body.appendChild(div)
+    
     return div
 }
 
-function criarSpan() {
-    let span = document.createElement('span')
-
-    let div = gerarDiv()
-
+function addSpanEButtonNaDiv() {
+    let input = receberInput()
     
+    if(input !== undefined){
+        let newDiv = gerarDiv()
+
+        let span = document.createElement('span')
+        span.innerHTML = `Texto digtado: ${input.value}, Contador: 0`
+
+        let bt = document.createElement('button')
+        bt.innerHTML = 'Zerar Contador'
+        bt.setAttribute('id', `${idBt + 1}`)
+        bt.setAttribute('onclick', 'zerarCont(id);event.stopPropagation()')
+
+        idBt += 1
+
+        newDiv.appendChild(span)
+        newDiv.appendChild(bt)  
+    }
 }
 
 function criarContador(){
-    
+    addSpanEButtonNaDiv()
+}
+
+function contar(id){
+    let contAux = document.getElementById(id).getAttribute('cont')
+    let input = document.getElementById(id).getAttribute('txt')
+    document.getElementById(id).firstChild.innerHTML = `Texto digtado: ${input}, Contador: ${parseInt(contAux) + 1}`
+    document.getElementById(id).setAttribute('cont', `${parseInt(contAux) + 1}`)
+}
+
+function zerarCont(id){
+    let div = document.getElementById(id)
+
+    let input = document.getElementById(id).getAttribute('txt')
+
+    div.setAttribute('cont', `0`)
+    div.firstChild.innerHTML = `Texto digtado: ${input}, Contador: 0`
+}
+
+function receberInput(){
+    if(document.getElementById('entrada').value !== ''){
+        return this.document.getElementById('entrada')
+    }else{
+        alert('Insira algo!')
+        document.location.reload(true)
+    }
 }
